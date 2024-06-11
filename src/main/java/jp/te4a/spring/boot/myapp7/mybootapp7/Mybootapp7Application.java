9a38854dc30a89eeda496919a6fb9301a5667b6b
Your Name <you@example.com>
2024-06-11 09:37:21 +0900

    課題７

--- src/main/java/jp/te4a/spring/boot/myapp7/mybootapp7/Mybootapp7Application.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/mybootapp7/Mybootapp7Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Mybootapp6Application {
+public class Mybootapp7Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Mybootapp6Application.class, args);
+		SpringApplication.run(Mybootapp7Application.class, args);
 	}
 
 }
