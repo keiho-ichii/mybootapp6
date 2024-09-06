2ca090ba7e6bd9c614d235ba3d740200a2396b99
市井 慧帆 <210275@jc-21.jp>
2024-09-06 13:36:54 +0900

    最初の登録

--- src/main/java/jp/te4a/spring/boot/myapp8/mybootapp8/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp8/mybootapp8/BookRepository.java
@@ -1,7 +1,9 @@
-package jp.te4a.spring.boot.myapp7.mybootapp7;
+package jp.te4a.spring.boot.myapp8.mybootapp8;
 
 import java.util.List;
 import java.util.ArrayList;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Repository;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,15 +14,24 @@ public class BookRepository {
 
     private final ConcurrentMap<Integer,BookBean>bookMap
                                   =new ConcurrentHashMap<>();
-public BookBean save(BookBean bookBean){
-    return bookMap.put(bookBean.getId(),bookBean);
-}
+    private int BOOK_ID = 1;
+    public int getBookid() {
+        return BOOK_ID++;
+    }                     
+public BookBean update(BookBean updateBookBean){
+    BookBean bookBean = bookMap.get(updateBookBean.getId());
+    BeanUtils.copyProperties(updateBookBean, bookBean);
+    return bookBean;
+ }
 
 public void delete(Integer bookId) {
     bookMap.remove(bookId);
-}
+ }
 
 public List<BookBean>findAll() {
     return new ArrayList<>(bookMap.values());
-}
+ }
+public BookBean findOne(Integer id) {
+    return bookMap.get(id);
+ }
 }
